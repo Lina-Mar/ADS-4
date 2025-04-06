2ec6374b567f939cebb649e1ec3193aa316c3831
Lina-Mar <martipoli7@gmail.com>
2025-04-06 21:56:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,70 +1,62 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    for (int j = i + 1; j < len; j++) {
-      if (arr[i] + arr[j] == value) {
-        count++;
+  int totalPairs = 0;
+  for (int idx1 = 0; idx1 < len; ++idx1) {
+    for (int idx2 = idx1 + 1; idx2 < len; ++idx2) {
+      if (arr[idx1] + arr[idx2] == value) {
+        totalPairs++;
       }
     }
   }
-  return count; 
+  return totalPairs;
 }
 
 int countPairs2(int *arr, int len, int value) {
-  int count = 0;
-  int left = 0;
-  int right = len - 1;
-  while (left < right) {
-    int sum = arr[left] + arr[right];
-    if (sum == value) {
-      if (arr[left] == arr[right]) {
-        int num = right - left + 1;
-        count += num * (num - 1) / 2;
-        break;
+  int totalPairs = 0;
+  int upperLimit = len - 1;
+  while (upperLimit > 0 && arr[upperLimit] > value) {
+    upperLimit--;
+  }
+  for (int lowerIdx = 0; lowerIdx < len; lowerIdx++) {
+    for (int upperIdx = upperLimit;
+      upperIdx > lowerIdx; upperIdx--) {
+        if (arr[lowerIdx] + arr[upperIdx] == value) {
+          totalPairs++;
+        }
       }
-      int left_val = arr[left];
-      int left_count = 0;
-      while (arr[left] == left_val && left < right) {
-        left++;
-        left_count++;
+  }
+  return totalPairs;
+}
+
+int searchBound(int* dataArr, int startIdx, int endIdx, int searchKey, bool findFirst) {
+  int foundPos = -1;
+  while (startIdx <= endIdx) {
+    int midIdx = startIdx + (endIdx - startIdx) / 2;
+    if (dataArr[midIdx] == searchKey) {
+      foundPos = midIdx;
+      if (findFirst) {
+        endIdx = midIdx - 1;
+      } else {
+        startIdx = midIdx + 1;
       }
-      int right_val = arr[right];
-      int right_count = 0;
-      while (arr[right] == right_val && right >= left) {
-        right--;
-        right_count++;
-      }
-      count += left_count * right_count;
-    }
-    else if (sum < value) {
-      left++;
-    }
-    else {
-      right--;
+    } else if (dataArr[midIdx] < searchKey) {
+      startIdx = midIdx + 1;
+    } else {
+      endIdx = midIdx - 1;
     }
   }
-  return count;
+  return foundPos;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count = 0;
-  for (int i = 0; i < len - 1; i++) {
-    int complement = value - arr[i];
-    int pos = binarySearch(arr, i + 1, len - 1, complement);
-    if (pos != -1) {
-      count++;
-      int j = pos - 1;
-      while (j > i && arr[j] == complement) {
-        count++;
-        j--;
-      }
-      j = pos + 1;
-      while (j < len && arr[j] == complement) {
-        count++;
-        j++;
-      }
+  int pairCounter = 0;
+  for (int idx = 0; idx < len; idx++) {
+    int diffValue = value - arr[idx];
+    int firstOccur = searchBound(arr, idx + 1, len - 1, diffValue, true);
+    if (firstOccur != -1) {
+      int lastOccur = searchBound(arr, idx + 1, len - 1, diffValue, false);
+      pairCounter += (lastOccur - firstOccur + 1);
     }
   }
-  return count;
+  return pairCounter;
 }
